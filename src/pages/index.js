aa96466336ea11d6cf87e53e3e41bf7cc20403e2
LuAnderson <lucasand96@gmail.com>
2020-05-20 11:07:47 -0300

    feat(home.p) implementa service e novos components na pagina principal

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import {
-  Logo, 
+import { 
+  Grid,
 } from '../components';
 
+import * as UtilService from '../services/utils';
+import * as Product from '../services/mock'
+
 function HomePage() {
+
+  const [productList, setProductList] = useState(Product.itens);
+  const [isMobile, setIsMobile] = useState();
+
+  useEffect(() => {
+    setIsMobile(UtilService.deviceIsMobile());
+  }, []);
+
   return (
-    <Logo/>
-  );
+    <Grid 
+      list={productList}
+      isMobile={isMobile}
+    />
+  )
 }
 
 export default HomePage;
