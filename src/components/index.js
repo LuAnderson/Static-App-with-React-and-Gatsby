d7c6874ac4179a0ea1d5ca4c6e5bed7a4224d106
LuAnderson <lucasand96@gmail.com>
2020-06-01 12:09:18 -0300

    feat(component): adiciona declaração de novos components

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,13 @@
-import Grid from './grid';
 import Cart from './cart';
+import Banner from './banner';
+import Footer from './footer';
+import Grid from './grid';
+import Header from './header';
 
 export {
     Cart,
+    Banner,
+    Footer,
     Grid,
+    Header,
 };
