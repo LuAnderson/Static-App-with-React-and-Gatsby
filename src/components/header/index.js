f31735036d2ee8a4230ae87c33cd3d83efb45d59
LuAnderson <lucasand96@gmail.com>
2020-06-21 11:23:31 -0300

    feat(header.c): adiciona chamadas de filtros de texto - card

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,14 +3,21 @@ import PropTypes from 'prop-types';
 
 import * as S from './styles';
 
-function Header({ countCart }) {
+function Header({ countCart, handleChange, searchTerm }) {
   return (
     <S.Header>
       <S.List>
         <S.Anchor href="http://lucasanderson.com.br/" target="_blank">
           <S.Logo src="https://luanderson.github.io/AnDesenv/images/logo.png" alt=""></S.Logo>
         </S.Anchor>
-        <S.Search type="search" id="search" name="search" placeholder="Procurar produtos"/>
+        <S.Search 
+          id="search" 
+          name="search" 
+          onChange={handleChange}
+          placeholder="O que você está procurando?"
+          type="search" 
+          value={searchTerm}
+          />
         <S.Item>Carrinho 
           {countCart > 0 && <S.Count>{countCart}</S.Count>}
         </S.Item>
@@ -21,6 +28,8 @@ function Header({ countCart }) {
 
 Header.propTypes = {
   countCart: PropTypes.number.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  searchTerm: PropTypes.string.isRequired,
 };
 
 export default Header;
