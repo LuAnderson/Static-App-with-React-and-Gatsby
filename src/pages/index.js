f2b259793f98aa0fdda42d02402838df9d4c7367
LuAnderson <lucasand96@gmail.com>
2020-06-21 11:22:27 -0300

    feat(home.p): implementa filtro de produtos via busca

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,19 +12,34 @@ import * as Product from '../services/mock'
 
 function HomePage() {
 
-  const [productList, setProductList] = useState(Product.mock.catalog);
+  const [productList, setProductList] = useState([]);
   const [isMobile, setIsMobile] = useState();
   const [countCart, setCountCart] = useState(0);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     setIsMobile(UtilService.deviceIsMobile());
-  }, []);
+
+    const searchItem = Product.mock.catalog.filter(item =>
+      item.name.toLowerCase().includes(searchTerm)
+    );
+
+    setProductList(searchItem);
+  }, [searchTerm]);
 
   const addCount = () => setCountCart(countCart + 1);
 
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value.toLowerCase());
+  };
+
   return (
     <>
-      <Header countCart={countCart} />
+      <Header 
+        countCart={countCart} 
+        handleChange={handleChange} 
+        searchTerm={searchTerm} 
+      />
       <Banner urls={Product.mock.banner} />
       <Grid 
         addCart={addCount}
