17ff0e041b1d1b9b3343d5cb196554b5b99747b6
LuAnderson <lucasand96@gmail.com>
2020-06-21 23:10:10 -0300

    feat(backup-response): salva objeto comentado para usos futuros

---
+++ src/services/backup-response.js
@@ -0,0 +1,97 @@
+// ! IMPORTANTE - Copia do Response 
+
+/*
+{
+"banner":[
+   "https://www.traininginstitutepune.in/wp-content/uploads/2019/10/react-js-banner.png",
+   "https://i0.wp.com/wp-and-react.com/wp-content/uploads/2019/04/gatsby.jpg?fit=2128%2C896&ssl=1",
+   "https://www.joaopedro.cc/img/upload/blog-gatsby-react-2.png"
+],
+"catalog":[
+   {
+      "id":1,
+      "name":"Iphone 11",
+      "description":"- Apple com 64GB, Tela Retina HD de 6,1",
+      "price":3.999,
+      "photo":"https://images-na.ssl-images-amazon.com/images/I/71iO2R%2BCLjL._AC_SX425_.jpg"
+   },
+   {
+      "id":2,
+      "name":"Samsung Galaxy s10+",
+      "description":"- 128Gb dual chip android",
+      "price":2.689,
+      "photo":"https://www.casasbahia-imagens.com.br/TelefoneseCelulares/Smartphones/Android/14817289/1087843024/smartphone-samsung-galaxy-s10-azul-128gb-8gb-ram-tela-infinita-6-4-camera-traseira-tripla-powershare-leitor-digital-na-tela-android-9-0-14817289.jpg"
+   },
+   {
+      "id":3,
+      "name":"Notebook Gamer Dell G5-5590",
+      "description":"- 9ª Intel Core I7 16GB 512GB SSD W10",
+      "price":8.652,
+      "photo":"https://a-static.mlcdn.com.br/1500x1500/notebook-gamer-dell-g5-5590-m80p-9a-geracao-intel-core-i7-16gb-512gb-ssd-placa-video-nvidia-rtx-2060-15-6-windows-10/dell/cg5590w7082pbr/504e08268009f9624ffe829f749fd605.jpg"
+   },
+   {
+      "id":4,
+      "name":"Xbox One X",
+      "description":"- 1Tb 4K",
+      "price":3.046,
+      "photo":"https://images-americanas.b2w.io/produtos/01/00/img/47415/1/47415132_1GG.jpg"
+   },
+   {
+      "id":5,
+      "name":"Smartwatch Samsung Galaxy Watch Active2",
+      "description":"- Preto",
+      "price":1.549,
+      "photo":"https://images-americanas.b2w.io/produtos/01/00/img/360560/6/360560642_1GG.jpg"
+   },
+   {
+      "id":6,
+      "name":"Smart TV LED 55 Sony KD 55X705F",
+      "description":"- 2 HDMI 3 USB Wi-Fi",
+      "price":3.988,
+      "photo":"https://www.pontofrio-imagens.com.br/tv-video/Televisores/SmartTV/13465240/1293110319/smart-tv-led-55-quot-sony-xbr-55x905-4k-hdr-com-android-wi-fi-3-usb-4-hdmix-motion-x-tended-dynamic-controle-comando-de-voz-13465240.jpg"
+   },
+   {
+      "id":7,
+      "name":"Cadeira Gamer Profissional Tgc12",
+      "description":"- Preta Thunderx3",
+      "price":1.752,
+      "photo":"https://http2.mlstatic.com/cadeira-gamer-profissional-tgc12-preta-thunderx3-D_NQ_NP_948291-MLB31044081876_062019-F.jpg"
+   },
+   {
+      "id":8,
+      "name":"Smart Speaker Intelbras Izy Mini Speak!",
+      "description":"- Alexa Integrada, BT, Wi-Fi, Cmd de voz, 2W RMS",
+      "price":299,
+      "photo":"https://www.miranda.com.br/lojas/2057/img/interno/prd_221280_i_produto_imagem_codprod_42042.jpg"
+   },
+   {
+      "id":9,
+      "name":"Monitor LG 29' UltraWide",
+      "description":"- IPS HDMI Full HD ScreenSplit Preto",
+      "price":1.621,
+      "photo":"https://www.gigantec.com.br/media/catalog/product/cache/1/image/800x925/9df78eab33525d08d6e5fb8d27136e95/m/o/monitor_lg_gamer_full_hd_ultrawide_ips_hdr_29_hdmi_29wk600_2.jpg"
+   },
+   {
+      "id":10,
+      "name":"Box Comemorativo 20 Anos De Harry Potter",
+      "description":"- 7 Livros Capa Dura Com Poster Grátis - 1ª Ed.",
+      "price":499,
+      "photo":"https://images-americanas.b2w.io/produtos/01/00/img/1611964/8/1611964896_1SZ.jpg"
+   },
+   {
+      "id":11,
+      "name":"Bicicleta Aro 29",
+      "description":"- Ksw Shimano 24 Vel A Disco Ltx",
+      "price":1.299,
+      "photo":"https://i.pinimg.com/originals/33/95/7f/33957f68dc1d22f59a20f311166814de.png"
+   },
+   {
+      "id":12,
+      "name":"AirPods com estojo de recarga sem fio",
+      "description":"- Apple",
+      "price":1.679,
+      "photo":"https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MRXJ2?wid=1144&hei=1144&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1551489675083"
+   }
+]
+};
+*/
